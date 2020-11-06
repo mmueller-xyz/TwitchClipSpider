91264d5ca96688ed4c07b918329b854b9cce7e56
mmueller-xyz <maximilian@mmueller.xyz>
2020-11-06 15:46:12 +0100

    initial commit

---
+++ downloader.py
@@ -0,0 +1,47 @@
+#!python3
+import argparse
+import os
+
+import requests
+import threading
+from time import sleep
+
+TWITCH_URL = "https://clips-media-assets2.twitch.tv/"
+TIMEOUT = 10*60*60 # 10 hours in seconds
+
+
+def get_url(vod_id, offset):
+    return f"{TWITCH_URL}{vod_id}-offset-{offset}.mp4"
+
+
+def get_fname(vod_id, offset):
+    return f"./clips/{vod_id}-offset-{offset}.mp4"
+
+
+def dl_clip(url, file):
+    r = requests.request("GET", url)
+    if r.status_code == 200:
+        print(f"found clip {file}")
+        with open(file, "wb") as f:
+            f.write(r.content)
+
+
+def check_vod(vod_id, start_offset, end_offset):
+    try:
+        os.mkdir("./clips")
+    except OSError:
+        pass
+
+    for i in range(start_offset,end_offset):
+        threading.Thread(target=dl_clip, args=(get_url(vod_id, i), get_fname(vod_id, i))).start()
+        sleep(.05)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sid", metavar="Stream_ID", type=int)
+    parser.add_argument("s", metavar="Start_Offset", nargs="?", default=0, type=int)
+    parser.add_argument("e", metavar="End_Offset", nargs="?", default=TIMEOUT, type=int)
+    args = parser.parse_args()
+    check_vod(args.sid, args.s, args.e)
